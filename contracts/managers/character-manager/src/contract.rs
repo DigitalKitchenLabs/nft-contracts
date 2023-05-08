100a5573a0b7901b5797618cf7da78c9afa3dc60
keyleu <kleuyckx@gmail.com>
2023-05-08 18:45:50 +0100

    Added entry point for query

--- contracts/managers/character-manager/src/contract.rs
+++ contracts/managers/character-manager/src/contract.rs
@@ -761,6 +761,7 @@ pub fn update_config(
     Ok(Response::new().add_attribute("action", "update_config"))
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: ManagerQueryMsg) -> StdResult<Binary> {
     match msg {
         ManagerQueryMsg::Config {} => to_binary(&query_config(deps)?),
